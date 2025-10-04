cf9822577906b91ffc0ab5169b3633349b66f1f3
WILDER <wildersantamaria14@gmail.com>
2025-10-03 19:44:43 -0500

    Add project files

--- src/java/dbase/Conexion.java
+++ src/java/dbase/Conexion.java
@@ -4,10 +4,27 @@
  */
 package dbase;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author USER
  */
 public class Conexion {
-    
+    public static Connection MySQL() throws ClassNotFoundException, SQLException {
+        try {
+            // Intentar con el driver nuevo primero
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            // Si no funciona, intentar con el driver antiguo
+            Class.forName("com.mysql.jdbc.Driver");
+        }
+        
+        Connection c=DriverManager.getConnection
+                                  ("jdbc:mysql://localhost:3306/bd_example?useSSL=false&serverTimezone=UTC","root","123456");
+
+        return c;
+    }
 }
--- src/java/vo/Producto.java
+++ src/java/vo/Producto.java
@@ -1,13 +1,48 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package vo;
 
-/**
- *
- * @author USER
- */
-public class Producto {
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+public class Producto implements Serializable {
+    private static final long serialVersionUID = 1L;
     
+    private int id;
+    private String modelo;
+    private BigDecimal precio;
+    private String almacenamiento;
+    private String color;
+    private int stock;
+    
+    // Constructores
+    public Producto() {}
+    
+    public Producto(int id, String modelo, BigDecimal precio, 
+                   String almacenamiento, String color, int stock) {
+        this.id = id;
+        this.modelo = modelo;
+        this.precio = precio;
+        this.almacenamiento = almacenamiento;
+        this.color = color;
+        this.stock = stock;
+    }
+    
+    // Getters y Setters
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+    
+    public String getModelo() { return modelo; }
+    public void setModelo(String modelo) { this.modelo = modelo; }
+    
+    public BigDecimal getPrecio() { return precio; }
+    public void setPrecio(BigDecimal precio) { this.precio = precio; }
+    
+    public String getAlmacenamiento() { return almacenamiento; }
+    public void setAlmacenamiento(String almacenamiento) { this.almacenamiento = almacenamiento; }
+    
+    public String getColor() { return color; }
+    public void setColor(String color) { this.color = color; }
+    
+    public int getStock() { return stock; }
+    public void setStock(int stock) { this.stock = stock; }
 }
