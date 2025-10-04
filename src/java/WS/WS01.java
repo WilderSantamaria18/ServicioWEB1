caefbdf37d4232b72d8ae70fe4dc63c37343fc4b
WILDER <wildersantamaria14@gmail.com>
2025-10-03 19:30:35 -0500

    segsegundo cambio

---
+++ src/java/WS/WS01.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package WS;
+
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebService;
+
+/**
+ *
+ * @author USER
+ */
+
+public class WS01 {
+    
+
+}
